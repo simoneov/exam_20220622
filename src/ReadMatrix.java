7ebde508ea2d86c2e5c6378708947d4037166160
Davide Mastroianni <davide.mastroianni@euris.it>
2022-06-22 09:18:12 +0200

    fixes

--- src/ReadMatrix.java
+++ src/ReadMatrix.java
@@ -1,28 +1,16 @@
 
 /*
-  Given two matrix of ints prints on standard output the sum of both matrix
-  A: {
+  Given a string as standard input parse the string into a matrix
+
+  args[0]: "{{1,2,3},{1,2,3},{1,2,3}}"
+
+  RESULT: {
     {1, 2, 3},
     {1, 2, 3},
     {1, 2, 3}
   }
-
-  B: {
-    {1, 2, 3},
-    {1, 2, 3},
-    {1, 2, 3}
-  }
-
-  RESULT:
-  {
-    {2, 4, 6},
-    {2, 4, 6},
-    {2, 4, 6}
-  }
-
-  Result matrix must be printed and formatted as previously shown
  */
-public class SumTwoMatrix {
+public class ReadMatrix {
 
   public static void main(String[] args) {
 
--- src/SumTwoMatrix.java
+++ src/SumTwoMatrix.java
@@ -1,14 +1,26 @@
 
 /*
-  Given a string as standard input parse the string into a matrix
-
-  args[0]: "{{1,2,3},{1,2,3},{1,2,3}}"
-
-  RESULT: {
+  Given two matrix of ints prints on standard output the sum of both matrix
+  A: {
     {1, 2, 3},
     {1, 2, 3},
     {1, 2, 3}
   }
+
+  B: {
+    {1, 2, 3},
+    {1, 2, 3},
+    {1, 2, 3}
+  }
+
+  RESULT:
+  {
+    {2, 4, 6},
+    {2, 4, 6},
+    {2, 4, 6}
+  }
+
+  Result matrix must be printed and formatted as previously shown
  */
 public class SumTwoMatrix {
 
